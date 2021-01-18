9c1e677f3c13d55ff49e404a894e86a28b42285a
xiatianxiao <xiatianxiao@baidu.com>
2021-01-18 18:30:48 +0800

    add xor function between forwards

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,14 @@
 */
 use std::thread;
 use async_std::net::{Incoming, TcpListener, TcpStream};
-use async_std::io::{BufRead, BufReader, BufWriter, Write};
+use async_std::io::{BufReader, BufWriter};
 use async_std::prelude::*;
-// use std::io::ErrorKind;
 use async_std::task;
 use futures::future::FutureExt;
 use futures::{pin_mut, select, future::select_all, future,io::{ReadHalf, WriteHalf, Read},};
 use futures::stream::FusedStream;
-use std::net::ToSocketAddrs;
 use futures::stream::StreamExt;
+use std::net::ToSocketAddrs;
 
 pub type EcResult<T> = Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
 
@@ -29,76 +28,70 @@ pub async fn read_stream(mut reader: &mut BufReader<&TcpStream>) -> io::Result<B
     }
 }
 
+/*
+    read_xor_stream paired with write_xor_stream
+*/
+pub async fn read_xor_stream(mut reader: &mut BufReader<&TcpStream>) -> io::Result<Box<[u8]>> {
+    let mut buffer: Vec<u8> = vec![0; 1024];
+    match reader.read(&mut buffer).await {
+        Ok(len) => {
+            buffer.truncate(len);
+            buffer.iter_mut().for_each(|x| *x ^= 0x55);
+            Ok(buffer.into_boxed_slice())
+        },
+        Err(err) => Err(err),
+    }
+}
+
 pub async fn do_forward(mut stream: TcpStream, up_addr: &str) ->  EcResult<()> {
     println!("connecting: {}", stream.peer_addr()?);
-    let (hreader, mut hwriter) = (&stream, &stream);
 
-    let mut reader = BufReader::new(hreader);
-    let mut writer = BufWriter::new(hwriter);
-    
-    let mut up_stream = TcpStream::connect("www.baidu.com:80").await?;
+    let (reader, mut writer) = (&stream, &stream);
+    let mut reader = BufReader::new(reader);
+    let mut writer = BufWriter::new(writer);
+
+    let mut up_stream = TcpStream::connect(up_addr).await?;
+
     let (up_reader, mut up_writer) = (&up_stream, &up_stream);
-    let up_reader = BufReader::new(up_reader);
+    let mut up_reader = BufReader::new(up_reader);
     let mut up_writer = BufWriter::new(up_writer);
-    let mut up_lines = up_reader.lines().fuse();
 
     loop {
-        let mut buffer: Vec<u8> = vec![0; 1024];
 
         select! {
-            // line = reader.read(&mut buffer).fuse() => match line{
-                line = read_stream(&mut reader).fuse() => match line{
-                /*
-                None => {
-                    println!("Connection was closed by server");
-                    break;
-                },
-                Some(line) => {
-                    if let Err(e) = line{
-                        println!("err {:?}", e);
-                        break;
-                    }
-                    let r = line.unwrap();
-                    println!("in data:[{:?}], {:?}", r, r.len());
-                    // handle_server_line(line?),
-                    {
-                        up_writer.write_all((r+"\n").as_bytes()).await?;
-                        up_writer.flush().await?;
-                    }
-                },
-                */
+            line = read_stream(&mut reader).fuse() => match line{
+
                 Ok(data) => {
-                    // buffer.truncate(data);
-                    // let buf = buffer.into_boxed_slice();
+
                     up_writer.write_all(&data).await?;
                     up_writer.flush().await?;
-                    println!("in ok: [{:?}]", data);
+                    let a = String::from_utf8((&data).to_vec());
+                    // println!("in ok: [{:?}]", a);
+                    if data.len() == 0 {
+                        break;
+                    }
                 },
                 Err(e) => {
                     println!("in err: {:?}", e);
                     break;
                 }
             },
-            line = up_lines.next() => match line{
-                None => {
+            line = read_stream(&mut up_reader).fuse() => match line{
+                Err(e) => {
                     println!("Connection was closed by remote peer");
                     break;
                 },
-                Some(line) => {
-                    if let Err(e) = line{
-                        println!("remote err {:?}", e);
+                Ok(data) => {
+
+                    writer.write_all(&data).await?;
+                    writer.flush().await?;
+                    let a = String::from_utf8((&data).to_vec());
+                    // println!("remote data:[{:?}]", a);
+                    if data.len() == 0 {
                         break;
                     }
-                    println!("remote data:[{:?}]", line);
-                    // handle_server_line(line?),
-                    writer.write_all(line.unwrap().as_bytes());
-                    writer.flush();
-                },
+                }
             },
-            // line = stdin_lines.next() => match line{
-            //     None => break,
-            //     Some(line) => handle_stdin_line(line?).await?,
-            // },
         }    
     }
     Ok(())
@@ -112,10 +105,11 @@ async fn start_server() -> std::io::Result<()> {
     let mut incoming: Incoming = server.incoming();
 
     while let Some(stream) = incoming.next().await {
-        let stream : TcpStream = stream?;
+        let mut stream : TcpStream = stream?;
+
         task::spawn(async{
             println!("incoming");
-            if let Err(s) = do_forward(stream, "172.105.242.229:80").await{
+            if let Err(s) = do_forward(stream, "172.105.242.229:8081").await{
                 println!("connect forward failed: {:?}", s);
             }
             println!("incoming done");
